d050cb14edeca9a7e0ad4b895fc5844dddeabfa5
BuildTools <unconfigured@null.spigotmc.org>
2021-02-02 17:27:55 +0000

    add gif

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import { createStore } from "redux";
 import reducer from "../reducers/reducer";
 
+// This one object is the source of truth.
 const store = createStore(reducer);
 
 export default store;
